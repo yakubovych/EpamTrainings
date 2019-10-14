0c332d9d0062bc2d324f2c6c294792f10cd00fd2
Roman Yakubovych <yakubovychr@gmail.com>
2019-10-15 01:57:24 +0300

    fix conflicts with exceptions

--- HomeworkExceptions/OutputTasks.cs
+++ HomeworkExceptions/OutputTasks.cs
@@ -18,6 +18,37 @@
             try
             {
                 GeneratorOfExceptions.GenerateStackOverflowException();
+            }
+            catch(StackOverflowException e)
+            {
+                Output.Write(e.Message);
+            }
+
+            Output.Write("----------------------------");
+
+            Output.Write("\n--- Task2 ---");
+            try
+            {
+                GeneratorOfExceptions.GenerateIndexOutOfRangeException();
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Output.Write(e.Message);
+            }
+
+            Output.Write("----------------------------");
+
+            Output.Write("\n--- Task3 ---");
+
+            Output.Write("Generated exceptions logged in event viewer. Add screenshot to the project 'Task3EventViewer.png'");
+
+            Output.Write("----------------------------");
+
+            Output.Write("\n--- Task4 ---");
+
+            try
+            {
+                GeneratorOfExceptions.GenerateStackOverflowException();
                 GeneratorOfExceptions.GenerateIndexOutOfRangeException();
             }
             catch (StackOverflowException e)
@@ -31,7 +62,8 @@
 
             Output.Write("----------------------------");
 
-            Output.Write("\n--- Task2 ---");
+            Output.Write("\n--- Task5 ---");
+
             try
             {
                 GeneratorOfExceptions.DoSomeMath(-1, 2);
