8b71d58ffa4b084b9a9b88066b9f3854193d2697
Roman Yakubovych <yakubovychr@gmail.com>
2019-10-15 12:35:43 +0300

    add tasks with iostreams

--- OutputOfTasks/Program.cs
+++ OutputOfTasks/Program.cs
@@ -12,6 +12,7 @@
             var runnerStructures = new HomeworkStructures.OutputTasks(new Output());
             var runnerEnums = new HomeworkEnums.OutputTasks(new Output());
             var runnnerExceptions = new HomeworkExceptions.OutputTasks(new Output());
+            var runnerIoStreams = new HomeworkIoStreams.OutputTasks(new Output());
 
             while (!isCheckedHomework)
             {
@@ -21,6 +22,7 @@
                     "(1) Check tasks with Structures;\n" +
                     "(2) Check tasks with Enums;\n" +
                     "(3) Check tasks with Exceptions;\n" +
+                    "(4) Check tasks with IOStreams;\n" +
                     "(0)  Exit.\n");
                 Console.WriteLine("What action do you choose?");
                 string number = Console.ReadLine();
@@ -38,6 +40,11 @@
                         break;
                     case "3":
                         runnnerExceptions.RunTasks();
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case "4":
+                        runnerIoStreams.RunTasks();
                         Console.ReadKey();
                         Console.Clear();
                         break;
