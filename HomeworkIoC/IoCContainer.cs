a0d114312934fef57370e69a635fd953a285d9de
Roman Yakubovych <yakubovychr@gmail.com>
2019-12-07 20:32:53 +0200

    add homework with ioc container

---
+++ HomeworkIoC/IoCContainer.cs
@@ -0,0 +1,46 @@
+﻿namespace HomeworkIoC
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Container
+    {
+
+        private readonly Dictionary<Type, Func<object>> _registeredTypes
+        = new Dictionary<Type, Func<object>>();
+
+        public void Register<TIn, TOut>() where TOut : TIn
+        {
+            _registeredTypes.Add(typeof(TIn), () => GetInstance(typeof(TOut)));
+        }
+
+        public T GetInstance<T>()
+        {
+            return (T)GetInstance(typeof(T));
+        }
+
+        public void RegisterSingleton<T>(T obj)
+        {
+            _registeredTypes.Add(typeof(T), () => obj);
+        }
+
+        public object GetInstance(Type type)
+        {
+            if (_registeredTypes.ContainsKey(type))
+            {
+                return _registeredTypes[type]();
+            }
+
+            var constructor = type.GetConstructors()
+                .OrderByDescending(c => c.GetParameters().Length)
+                .FirstOrDefault();
+
+            var args = constructor?.GetParameters()
+                .Select(param => GetInstance(param.ParameterType))
+                .ToArray();
+
+            return Activator.CreateInstance(type, args);
+        }
+    }
+}
---
+++ IoCTests/ContainerTests.cs
@@ -0,0 +1,152 @@
+namespace IoCTests
+{
+    using HomeworkIoC;
+    using NUnit.Framework;
+
+    public class ContainerTestBase
+    {
+        protected Container Container;
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            Container = new Container();
+        }
+
+        [TearDown]
+        public void AfterEach()
+        {
+            Container = null;
+        }
+    }
+
+    [TestFixture]
+    public class Container_GetInstance : ContainerTestBase
+    {
+        [Test]
+        public void CreatesAnInstanceWithNoParams()
+        {
+            var subject = (A)Container.GetInstance(typeof(A));
+
+            Assert.IsInstanceOf<A>(subject);
+        }
+
+        [Test]
+        public void CreatesAnInstanceWithParams()
+        {
+            var subject = (B)Container.GetInstance(typeof(B));
+
+            Assert.IsInstanceOf<A>(subject.A);
+        }
+
+        [Test]
+        public void ItAllowsAParameterlessConstructor()
+        {
+            var subject = (C)Container.GetInstance(typeof(C));
+
+            Assert.IsTrue(subject.Invoked);
+        }
+
+        [Test]
+        public void ItAllowsGenericInitialization()
+        {
+            var subject = Container.GetInstance<A>();
+
+            Assert.IsInstanceOf<A>(subject);
+        }
+
+        internal class A
+        {
+        }
+
+        internal class B
+        {
+            public A A { get; set; }
+
+            public B(A a)
+            {
+                A = a;
+            }
+        }
+
+        internal class C
+        {
+            public bool? Invoked { get; set; }
+
+            public C()
+            {
+                Invoked = true;
+            }
+        }
+    }
+
+    [TestFixture]
+    public class Container_Register : ContainerTestBase
+    {
+        [Test]
+        public void RegisterATypeFromAnInterface()
+        {
+            Container.Register<IMaterial, Plastic>();
+
+            var subject = Container.GetInstance<IMaterial>();
+
+            Assert.IsInstanceOf<Plastic>(subject);
+        }
+
+        [Test]
+        public void InitializeObjectWithDependencies()
+        {
+            Container.Register<IMaterial, Toy>();
+
+            var subject = (Toy)Container.GetInstance<IMaterial>();
+
+            Assert.IsInstanceOf<Plastic>(subject.Material);
+        }
+
+        interface IMaterial
+        {
+            int Weight { get; }
+        }
+
+        class Plastic : IMaterial
+        {
+            public int Weight => 42;
+        }
+
+        class Metal : IMaterial
+        {
+            public int Weight => 84;
+        }
+
+        class Toy : IMaterial
+        {
+            public int Weight => 100;
+
+            public Plastic Material { get; set; } = null;
+
+            public Toy(Plastic plasticMaterial)
+            {
+                Material = plasticMaterial;
+            }
+        }
+    }
+
+    [TestFixture]
+    public class Container_RegisterSingleton : ContainerTestBase
+    {
+        [Test]
+        public void ItReturnsASingleInstance()
+        {
+            var pet = new Pet();
+            Container.RegisterSingleton(pet);
+
+            var subject = Container.GetInstance<Pet>();
+
+            Assert.AreEqual(pet, subject);
+        }
+
+        class Pet
+        {
+        }
+    }
+}
